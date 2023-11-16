1694b1f67492d65ac6fbb0dfa7c22eeeec55590a
seyemaguette <seyem2210@gmail.com>
2023-11-16 12:59:55 +0000

    les fichiers static

--- blog/urls.py
+++ blog/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('articles.urls')),
     path('', include('users.urls')),
    
 ]
+#staticfiles
+if not settings.DEBUG:
+    urlpatterns += [static(settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT)]
+#media
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
