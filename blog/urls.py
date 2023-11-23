7e8a8b13fc3f036ee7fe0e336ecb1080d2721d5f
seyemaguette <seyem2210@gmail.com>
2023-11-23 17:10:28 +0000

    changement url.py

--- blog/urls.py
+++ blog/urls.py
@@ -24,8 +24,7 @@ urlpatterns = [
     path('', include('users.urls')),
    
 ]
-#staticfiles
-if not settings.DEBUG:
-    urlpatterns += [static(settings.MEDIA_URL ,document_root = settings.MEDIA_ROOT)]
+
+
 #media
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
